1346cbcd26a124317a007825a6ea1cf8601f8479
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2020-03-28 20:07:35 +0100

    add global styles and svg images

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,14 @@
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import Card from "../components/card"
+import React from 'react'
+// import logo from './logo.svg'
+// import "./App.css"
+import Card from './components/card'
+import GlobalStyle from './components/global-styles'
 
 function App() {
   return (
     <>
       <Card />
+      <GlobalStyle />
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,54 @@
-import React from "react"
+import React from 'react'
 // import ReactDOM from 'react-dom'
-import { useSpring, animated } from "react-spring"
+import { useSpring, animated } from 'react-spring'
 // import './styles.css' // Icons made by Freepik from www.flaticon.com
+import styled from 'styled-components'
+import imgCard1 from './card1.svg'
+import imgCard2 from './card2.svg'
+import imgCard3 from './card3.svg'
+import imgCard4 from './card4.svg'
+
+const BaseCard = styled(animated.img)`
+  position: absolute;
+  border-radius: 5px;
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  will-change: transform;
+`
+
+const Card1 = styled(BaseCard)`
+  min-width: 60ch;
+  min-height: 60ch;
+  width: 45vw;
+  height: 45vw;
+  max-width: 100ch;
+  max-height: 100ch;
+`
+
+const Card2 = styled(BaseCard)`
+  width: 25ch;
+  height: 25ch;
+`
+
+const Card3 = styled(BaseCard)`
+  opacity: 0.9;
+  width: 25ch;
+  height: 25ch;
+`
+
+const Card4 = styled(BaseCard)`
+  width: 25ch;
+  height: 25ch;
+`
+
+const Container = styled('div')`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
 const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
@@ -16,27 +63,12 @@ function Card() {
   }))
 
   return (
-    <div
-      class="container"
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
-    >
-      <animated.div
-        class="card1"
-        style={{ transform: props.xy.interpolate(trans1) }}
-      />
-      <animated.div
-        class="card2"
-        style={{ transform: props.xy.interpolate(trans2) }}
-      />
-      <animated.div
-        class="card3"
-        style={{ transform: props.xy.interpolate(trans3) }}
-      />
-      <animated.div
-        class="card4"
-        style={{ transform: props.xy.interpolate(trans4) }}
-      />
-    </div>
+    <Container onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+      <Card1 src={imgCard1} style={{ transform: props.xy.interpolate(trans1) }} />
+      <Card2 src={imgCard2} style={{ transform: props.xy.interpolate(trans2) }} />
+      <Card3 src={imgCard3} style={{ transform: props.xy.interpolate(trans3) }} />
+      <Card4 src={imgCard4} style={{ transform: props.xy.interpolate(trans4) }} />
+    </Container>
   )
 }
 
